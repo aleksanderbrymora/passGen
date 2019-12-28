56541ed6ccd93f27b7704ae8fa52b5feb39109b3
aleksanderbrymora <aleksander.australia@gmail.com>
2019-12-29 00:37:32 +1100

    added option to copy

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
 	let length = 6;
 	let password = '';
+	let copied = false;
+
 	const randLetter = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 	const randNumber = () => (Math.floor(Math.random() * 10)).toString();
 	const randomSymbol = () => '~!@_#$%^:*+&`|'.split('')[Math.floor(Math.random() * 14)];
 	const randoms = [randLetter, randNumber, randomSymbol];
+
+	const copyPass = () => {
+		const copyText = document.querySelector('#password');
+		const range = document.createRange();
+		range.selectNode(copyText);
+		window.getSelection().addRange(range);
+		document.execCommand('copy');
+		window.getSelection().removeAllRanges();
+		copied = true;
+		setTimeout(() => copied = false, 2000)
+	};
+
 	const generate = () => {
 		let pass = '';
 		for (let i = 0; i < length; i++) {
@@ -21,7 +35,9 @@
 		<label for="length">Set the length</label>
 		<input bind:value={length} on:change={generate} min="6" max="50" id="length" type="range">
 
-		<p>{password}</p>
+		<p id="password">{password}</p>
+		{#if password.length}<button on:click={copyPass}>Copy</button>{/if}
+		{#if copied} <p>Copied!</p> {/if}
 	</div>
 </main>
 
